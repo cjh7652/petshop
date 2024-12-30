0353c4e30a9553a21d6de81871a87ff4cb9e5e93
jeehyun <cjh7652@hanmail.net>
2024-12-30 14:23:35 +0900

    form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -199,8 +199,118 @@ const SignUp = () => {
 								<label htmlFor="birthArea" className='label-style'>생년월일</label>
 								<input ref={birthInputRef} type="text" id="birthArea" required size={8} value={birth} onChange={(event) => {setBirth(event.target.value)}} onBlur={handleBirth}/>
 								<span className={`mes-style ${messages.birth.color}`}>{messages.birth.text}</span>
+                <p className="help-style"><IoCheckmark /> - 를 제외한 8글자 ex) 19900101</p>
 							</div>
 						</li>
+            <li>
+              <br />
+              <hr />
+              <br />
+            </li> 
+            <li id="terms-section">
+              <input type="checkbox" id="allCheck" className="chick-style" checked={allChecked} onChange={handleAllCheck} />
+              <label htmlFor="allCheck">
+                이용약관 및 개인정보수집 및 이용, 쇼핑정보 수신(선택)에 모두 동의합니다.
+              </label>
+              <br />
+              <h3>[필수] 이용약관 동의</h3>
+              <div className="termsBox">
+                <p>
+                   ■ 수집하는 개인정보 항목 <br />
+                  회사는 수집한 개인정보를 다음의 목적을 위해 활용합니다.
+                </p>
+                <p>
+                ο 서비스 제공에 관한 계약 이행 및 서비스 제공에 따른 요금정산 콘텐츠 제공 , 구매 및 요금 결제 , 물품배송 또는 청구지 등 발송 <br />
+                ο 회원 관리<br />
+                회원제 서비스 이용에 따른 본인확인 , 개인 식별 , 연령확인 , 만14세 미만 아동 개인정보 수집 시 법정 <br />대리인 동의여부 확인 , 고지사항 전달 ο 마케팅 및 광고에 활용<br />
+                접속 빈도 파악 또는 회원의 서비스 이용에 대한 통계
+                </p>
+                <p>
+                 ■ 개인정보의 보유 및 이용기간
+                </p>
+
+                <p>
+                회사는 개인정보 수집 및 이용목적이 달성된 후에는 예외 없이 해당 정보를 지체 없이 파기합니다.
+                </p>
+              </div>
+              <p>
+                <span>이용약관에 동의하십니까?</span>
+                <input type="checkbox" id="terms-check1" className='check-style' />
+                <label htmlFor="terms-check1">  동의함</label>
+              </p>
+
+
+              <h3>[필수] 개인정보 수집 및 이용 동의</h3>
+              <div className="termsBox">
+                  <p>
+                  ■ 수집하는 개인정보 항목
+                  <br />
+                  회사는 회원가입, 상담, 서비스 신청 등등을 위해 아래와 같은 개인정보를 수집하고 있습니다.
+                  </p>
+                  <p>
+                  ο 수집항목 : 이름 , 생년월일 , 성별 , 로그인ID , 비밀번호 , 비밀번호 질문과 답변 , 자택 전화번호 , 자택 주소 , 휴대전화번호 , 이메일 , 직업 , 회사명 , 부서 , 직책 , 회사전화번호 , 취미 , 결혼여부 , 기념일 , 법정대리인정보 , 서비스 이용기록 , 접속 로그 , 접속 IP 정보 , 결제기록
+                  <br />
+                  ο 개인정보 수집방법 : 홈페이지(회원가입) , 서면양식
+
+                  </p>
+                  <br />
+                  <p>
+                  ■ 개인정보의 수집 및 이용목적 <br />
+                  회사는 수집한 개인정보를 다음의 목적을 위해 활용합니다.
+                  <br />
+                  ο 서비스 제공에 관한 계약 이행 및 서비스 제공에 따른 요금정산 콘텐츠 제공 , 구매 및 요금 결제 , 물품배송 또는 청구지 등 발송 <br />
+                  ο 회원 관리 회원제 서비스 이용에 따른 본인확인 , 개인 식별 , 연령확인 , 만14세 미만 아동 개인정보 수집 시 법정 대리인 동의여부 확인 , 고지사항 전달 <br />
+                  ο 마케팅 및 광고에 활용 <br />
+                  접속 빈도 파악 또는 회원의 서비스 이용에 대한 통계
+                  </p>
+                  <p>
+                  ■ 개인정보의 보유 및 이용기간
+                    <br />
+                    회사는 개인정보 수집 및 이용목적이 달성된 후에는 예외 없이 해당 정보를 지체 없이 파기합니다.
+                  </p>
+              </div>
+              <p>
+                <span>개인정보 수집 및 이용에 동의하십니까?</span>
+                
+                <input type="checkbox" id="terms-check2" className='check-style' />
+                <label htmlFor="terms-check2">  동의함</label>
+              </p>
+
+              <h3>[필수] 쇼핑정보 수신 동의</h3>
+              <div className="termsBox">
+                <p>
+                ■ 수집하는 개인정보 항목 <br />
+                회사는 회원가입, 상담, 서비스 신청 등등을 위해 아래와 같은 개인정보를 수집하고 있습니다.
+                <br />
+                <br />
+                ο 수집항목 : 이름 , 생년월일 , 성별 , 로그인ID , 비밀번호 , 비밀번호 질문과 답변 , 자택 전화번호 , 자택 주소 , 휴대전화번호 , 이메일 , 직업 , 회사명 , 부서 , 직책 , 회사전화번호 , 취미 , 결혼여부 , 기념일 , 법정대리인정보 , 서비스 이용기록 , 접속 로그 , 접속 IP 정보 , 결제기록 <br />
+                ο 개인정보 수집방법 : 홈페이지(회원가입) , 서면양식
+                </p>
+                <p>
+                ■ 개인정보의 수집 및 이용목적 <br />
+                <br />
+                회사는 수집한 개인정보를 다음의 목적을 위해 활용합니다.
+                <br />
+                <br />
+                ο 서비스 제공에 관한 계약 이행 및 서비스 제공에 따른 요금정산 콘텐츠 제공 , 구매 및 요금 결제 , 물품배송 또는 청구지 등 발송 <br />
+                ο 회원 관리 회원제 서비스 이용에 따른 본인확인 , 개인 식별 , 연령확인 , 만14세 미만 아동 개인정보 수집 시 법정 대리인 동의여부 확인 , 고지사항 전달 <br />
+                ο 마케팅 및 광고에 활용 <br />
+                접속 빈도 파악 또는 회원의 서비스 이용에 대한 통계 <br /><br />
+                </p>
+                <p>
+                ■ 개인정보의 보유 및 이용기간 <br /><br />
+                회사는 개인정보 수집 및 이용목적이 달성된 후에는 예외 없이 해당 정보를 지체 없이 파기합니다.
+                </p>
+              </div>
+              <p>
+                <span>SMS 수신을 동의하십니까?</span>
+                <input type="checkbox" id="terms-check3" className='check-style' />
+                <label htmlFor="terms-check3">  동의함</label>
+              </p>
+            </li>
+            <li className="submit-section">
+              <button className="submit-style" type="submit">회원가입</button>
+            </li>
           </ul>
         </fieldset>
       </form>
